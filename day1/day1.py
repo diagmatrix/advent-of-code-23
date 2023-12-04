8d42c654471f7fe62e80934f5eac3be9498b0013
diagmatrix <manuelgbal1@gmail.com>
2023-12-04 19:43:11 +0100

    Day 1 solution

---
+++ day1/day1.py
@@ -0,0 +1,43 @@
+""""Day 1 solution"""
+NUMBERS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+
+def num_to_digit(digit):
+    match digit:
+        case "one":
+            return str(1)
+        case "two":
+            return str(2)
+        case "three":
+            return str(3)
+        case "four":
+            return str(4)
+        case "five":
+            return str(5)
+        case "six":
+            return str(6)
+        case "seven":
+            return str(7)
+        case "eight":
+            return str(8)
+        case "nine":
+            return str(9)
+        case _:
+            return str(0)
+
+
+if __name__ == "__main__":
+    problem_input = open("input.txt", "r").readlines()
+    total = 0
+    for line in problem_input:
+        digits = []
+        for i, c in enumerate(line):
+            if c.isdigit():
+                digits.append(c)
+            else:
+                for num in NUMBERS:
+                    if line[i:].startswith(num):
+                        digits.append(num_to_digit(num))
+        total += int(digits[0] + digits[-1])
+
+    print(f'Answer: {total}')
