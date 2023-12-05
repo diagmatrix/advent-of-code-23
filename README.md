6046248b8c43c969d3ea51b5288f45c0981cc13c
diagmatrix <manuelgbal1@gmail.com>
2023-12-06 00:23:13 +0100

    Day 3 part 2

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ My solutions to the advent of code 2023 challenge.
 | --- | ------ | ------ |
 | 1 | [day 1 solution](day1/day1.py) | It was tricky to realize that the words had to be parsed secuentially to transform them into digits. |
 | 2 | [day 2 solution](day2/day2.py) | |
+| 3 | [day 3 solution](day3/day3.py) | Part two was significantly easier to solve once I remembered `dict` exists |
--- day3/day3.py
+++ day3/day3.py
@@ -1,5 +1,5 @@
 """Day 3 solution"""
-SCHEMATIC = [line.strip() for line in open("input.txt", "r").readlines()]
+SCHEMATIC = [line.strip() for line in open("input.txt", "r").readlines()]  # Add input.txt in the directory
 ROWS = len(SCHEMATIC)
 COLUMNS = len(SCHEMATIC[0])
 
@@ -7,17 +7,19 @@ valid_positions = []
 
 def add_symbols(row_n, col_n):
     """Calculates the 'area of infulence' of a found symbol"""
+    positions = []
     for r in range(row_n-1, row_n+2):
         if 0 <= r <= ROWS:
             for c in range(col_n-1, col_n+2):
                 if 0 <= c <= COLUMNS:
-                    valid_positions.append((r,c))
+                    positions.append((r,c))
+    return positions
 
 # Parse valid positions
 for i in range(ROWS):
     for j, symbol in enumerate(SCHEMATIC[i]):
         if symbol not in '0123456789.':
-            add_symbols(i,j)
+            valid_positions += add_symbols(i,j)
 valid_positions = list(set(valid_positions))
 
 # Parse schematic for digits
@@ -38,4 +40,39 @@ for i, line in enumerate(SCHEMATIC):
                 valid_part = False
             digit_buffer = ""
 
-print(total)
+# Parse possible gears
+gears = []
+for i in range(ROWS):
+    for j, symbol in enumerate(SCHEMATIC[i]):
+        if symbol == '*':
+            gears.append({'parts': [], 'positions': add_symbols(i,j)})
+
+def add_digit_gear(digit_string, positions):
+    for gear in gears:
+        if bool(set(gear['positions']) & set(positions)):
+            gear['parts'].append(int(digit_string))
+
+# Add parts to possible gears
+for i, line in enumerate(SCHEMATIC):
+    digit_buffer = ""
+    digit_positions = []
+    for j, symbol in enumerate(line):
+        if symbol in '0123456789':
+            digit_buffer += symbol
+            digit_positions.append((i,j))
+            if j+1 == COLUMNS:
+                add_digit_gear(digit_buffer,digit_positions)
+        else:
+            if len(digit_buffer) > 0:
+                add_digit_gear(digit_buffer,digit_positions)
+            digit_buffer = ""
+            digit_positions = []
+
+# Get gear ratios
+gear_ratio_total = 0
+for gear in gears:
+    if len(gear['parts'])==2:
+        gear_ratio_total += gear['parts'][0] * gear['parts'][1]
+
+print(f'Total parts: {total}')
+print(f'Total gear ratio: {gear_ratio_total}')
